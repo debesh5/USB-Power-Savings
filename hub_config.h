32aaea287463274dc552ba81415ba8bd51ff5073
debesh5 <51858567+debesh5@users.noreply.github.com>
2026-06-11 16:54:13 -0700

    Add files via upload

---
+++ hub_config.h
@@ -0,0 +1,23 @@
+#define USB2_0 0.0015
+#define USB2_1 0.012
+#define USB2_2 0.48   // Assume UP has this minimally
+#define USB2_Eff 0.4  // Efficiency in each direction
+//
+#define USB3_0 4   //5G after 8b/10b
+#define USB3_1 10
+//
+
+#define USB3_2_WID 2
+#define USB3_3_WID 4  //Speculative
+//
+#define USB3_ENTRY_U1 0.1 // in usec
+#define USB3_EXIT_U1 4.0
+#define USB3_ENTRY_U2 10.0  // in usecs
+#define USB3_EXIT_U2 90.0
+#define USB3_ENTRY_U3 3000.0
+#define USB3_EXIT_U3 30000.0
+//
+#define USB3_POWER_U0 4.5 // Watts
+#define USB3_POWER_U1 1.0
+#define USB3_POWER_U2  0.1
+#define USB3_POWER_U3 0.0125
